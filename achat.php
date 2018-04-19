5d0d2b07fc0d2289abdbddca7bddb61e75c8ee3f
Tartanpion42 <38435909+Tartanpion42@users.noreply.github.com>
2018-04-19 12:28:41 +0200

    Update achat.php
    Mise en place de session, l'on peut acceder a la page achat que si l'on est connecter

--- achat.php
+++ achat.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -9,24 +12,29 @@
 	<?php include('nav.php'); ?>
 	<div class="conteneur">
 	<?php
-	/*Verification de la connexion*/
-	$connexion = mysqli_connect("localhost","root","","hardware");
+	if($_SESSION['connexion'] == 1) {	
+		/*Verification de la connexion*/
+		$connexion = mysqli_connect("localhost","root","","hardware");
 
-	if(mysqli_connect_error()){
-	printf("Echec de la connection : %s \n",mysqli_connect_error());
-		exit(); 
+		if(mysqli_connect_error()){
+		printf("Echec de la connection : %s \n",mysqli_connect_error());
+			exit(); 
+		}
+		$requete = "SELECT * FROM produit";
+		$res = mysqli_query($connexion,$requete);
+		//var_dump($res);
+		while($produit = mysqli_fetch_array($res,MYSQLI_BOTH)){
+			//echo"{$produit['id']} <br>";
+			echo "<hr/>";
+			echo"{$produit['name']} <br>";
+			echo"{$produit['titre']} <br>";
+			echo"{$produit['description']} <br>";
+		}
+		mysqli_close($connexion);
 	}
-	$requete = "SELECT * FROM produit";
-	$res = mysqli_query($connexion,$requete);
-	//var_dump($res);
-	while($produit = mysqli_fetch_array($res,MYSQLI_BOTH)){
-		//echo"{$produit['id']} <br>";
-		echo "<hr/>";
-		echo"{$produit['name']} <br>";
-		echo"{$produit['titre']} <br>";
-		echo"{$produit['description']} <br>";
+	else {
+		echo "Veuiller vous inscrire ou vous connecter";
 	}
-	mysqli_close($connexion);
 	?>
 	</div>
 <?php include('footer.html'); ?>
